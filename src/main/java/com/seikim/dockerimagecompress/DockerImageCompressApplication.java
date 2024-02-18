3e653c2215dd985de8ba50222c145e51ac2483b9
KIMSEI1124 <workju1124@gmail.com>
2024-02-19 08:01:04 +0900

    Init Project: Docker Image Compress

---
+++ src/main/java/com/seikim/dockerimagecompress/DockerImageCompressApplication.java
@@ -0,0 +1,13 @@
+package com.seikim.dockerimagecompress;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerImageCompressApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerImageCompressApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seikim/dockerimagecompress/DockerImageCompressApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seikim.dockerimagecompress;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerImageCompressApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
