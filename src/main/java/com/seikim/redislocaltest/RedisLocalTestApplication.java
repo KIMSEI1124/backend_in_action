fcd4cca0149db86f215571bdb1a30dec7c6adfb4
Sei Kim <workju1124@gmail.com>
2024-02-20 14:08:31 +0900

    Init Project

---
+++ src/main/java/com/seikim/redislocaltest/RedisLocalTestApplication.java
@@ -0,0 +1,13 @@
+package com.seikim.redislocaltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisLocalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisLocalTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seikim/redislocaltest/RedisLocalTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seikim.redislocaltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisLocalTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
