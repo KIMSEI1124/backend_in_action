d3e3e62c381805db16ee3cca575022ba286bc51f
KIMSEI1124 <workju1124@gmail.com>
2024-02-21 19:40:40 +0900

    Init Project

---
+++ src/test/java/com/seikim/httpclient/HttpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seikim.httpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
