1fed152d29728d44e85f4fcb50c4408fe65d909b
KIMSEI1124 <workju1124@gmail.com>
2024-02-22 08:18:15 +0900

    Init Project

---
+++ src/main/java/com/seikim/kopringbank/KopringBankApplication.java
@@ -0,0 +1,13 @@
+package com.seikim.kopringbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KopringBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KopringBankApplication.class, args);
+	}
+
+}
