a2e483d286287351991edbefe8ea67991b3f79e4
KIMSEI1124 <workju1124@gmail.com>
2024-02-27 08:39:01 +0900

    Init Project

---
+++ src/main/java/com/seikim/Jacobo/JacoboApplication.java
@@ -0,0 +1,13 @@
+package com.seikim.Jacobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacoboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JacoboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seikim/Jacobo/JacoboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seikim.Jacobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JacoboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
