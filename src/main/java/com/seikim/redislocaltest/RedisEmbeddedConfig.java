960df1dac9e1168394adea04e8f0894800920f2c
KIMSEI1124 <workju1124@gmail.com>
2024-02-26 09:43:09 +0900

    Feat: Redis Config 추가

--- src/main/java/com/seikim/redislocaltest/RedisEmbeddedConfig.java
+++ src/main/java/com/seikim/redislocaltest/RedisEmbeddedConfig.java
@@ -2,14 +2,18 @@ package com.seikim.redislocaltest;
 
 import jakarta.annotation.PostConstruct;
 import jakarta.annotation.PreDestroy;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import redis.embedded.RedisServer;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 
-@Profile("embedded-test")
+@Slf4j
+@Profile({"local", "embedded-test"})
 @Configuration
 public class RedisEmbeddedConfig {
     @Value("${spring.data.redis.port}")
@@ -19,7 +23,8 @@ public class RedisEmbeddedConfig {
 
     @PostConstruct
     private void start() throws IOException {
-        redisServer = new RedisServer(redisPort);
+        int port = isRedisRunning() ? findAvailablePort() : redisPort;
+        redisServer = new RedisServer(port);
         redisServer.start();
     }
 
@@ -28,5 +33,38 @@ public class RedisEmbeddedConfig {
         if (redisServer != null) {
             redisServer.stop();
         }
+    }
+
+    private boolean isRedisRunning() throws IOException {
+        return isRunning(executeGrepProcessCommand(redisPort));
+    }
+
+    public int findAvailablePort() throws IOException {
+        for (int port = 10000; port <= 65535; port++) {
+            Process process = executeGrepProcessCommand(port);
+            if (!isRunning(process)) {
+                return port;
+            }
+        }
+        throw new IllegalArgumentException("Not Found Available port: 10000 ~ 65535");
+    }
+
+    private Process executeGrepProcessCommand(int port) throws IOException {
+        String command = String.format("netstat -nat | grep LISTEN|grep %d", port);
+        String[] shell = {"/bin/sh", "-c", command};
+        return Runtime.getRuntime().exec(shell);
+    }
+
+    private boolean isRunning(Process process) {
+        String line;
+        StringBuilder pidInfo = new StringBuilder();
+        try (BufferedReader input = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
+            while ((line = input.readLine()) != null) {
+                pidInfo.append(line);
+            }
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+        return !pidInfo.isEmpty();
     }
 }
