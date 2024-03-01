175beadcfc5cc846915f1b27953a5be9c2d0036d
KIMSEI1124 <workju1124@gmail.com>
2024-03-01 22:16:55 +0900

    Init Project

---
+++ src/test/java/com/seikim/kotlinstarter/KotlinStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seikim.kotlinstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KotlinStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
