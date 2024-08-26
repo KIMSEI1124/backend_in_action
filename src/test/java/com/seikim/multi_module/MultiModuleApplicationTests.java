f4f712ece90d3658217d1aa2f5691bf5f42c7296
KIMSEI1124 <workju1124@gmail.com>
2024-08-26 14:21:32 +0900

    Chore(#6): Multi Module Project 초기 설정

---
+++ src/test/java/com/seikim/multi_module/MultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seikim.multi_module;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
