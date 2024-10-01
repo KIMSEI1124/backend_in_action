3bd78b32558174b114017f90929e5a37b50a9e48
KIMSEI1124 <workju1124@gmail.com>
2024-10-01 23:07:17 +0900

    Feat: Project 생성

---
+++ src/main/java/com/seikim/strategybean/StrategybeanApplication.java
@@ -0,0 +1,13 @@
+package com.seikim.strategybean;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategybeanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategybeanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seikim/strategybean/StrategybeanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seikim.strategybean;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategybeanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
